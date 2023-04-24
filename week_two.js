3d3ebd803becec4aa764d96aa5897d0e494f1d15
Zachary <gray.zachary1994@gmail.com>
2023-04-24 08:22:21 -0500

    Finished week 2 coding challenge.

---
+++ week_two.js
@@ -0,0 +1,26 @@
+/*Find the Smallest and Biggest Numbers
+
+Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
+Examples
+
+minMax([1, 2, 3, 4, 5]) ➞ [1, 5]
+
+minMax([2334454, 5]) ➞ [5, 2334454]
+
+minMax([1]) ➞ [1, 1]
+
+Notes
+
+All test arrays will have at least one element and are valid.*/
+
+function minMax(arr) {
+    let min = Math.min(...arr)
+	let max = Math.max(...arr)
+    return [min,max]
+}
+
+console.log(minMax([2334454, 5]))
+
+console.log(minMax([1]))
+
+console.log(minMax([1, 2, 3, 4, 5]))
